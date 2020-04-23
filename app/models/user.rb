95086cf5c5bca6f89a7f13e1757f351ee1ac1e67
enelesmai <xochitl.selene.fp@gmail.com>
2020-04-23 16:21:27 -0500

    added comment model and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :posts
+    has_many :comments
     validates :name, presence: true, length: { minimum: 2 }, uniqueness: true 
     validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*(\.[a-z]+)?\z/i, message: "use a valid email" }
 end

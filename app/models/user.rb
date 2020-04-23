19615d7d2923e7c7eb087ced471115c387fdd8f9
enelesmai <xochitl.selene.fp@gmail.com>
2020-04-23 15:49:05 -0500

    added post validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :posts
     validates :name, presence: true, length: { minimum: 2 }, uniqueness: true 
     validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*(\.[a-z]+)?\z/i, message: "use a valid email" }
 end

8f2b8fca3dd0db8c86a30a960e1433e330cc0ff9
enelesmai <xochitl.selene.fp@gmail.com>
2020-04-23 15:33:35 -0500

    created rails app and added validations to User model

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+    validates :name, presence: true, length: { minimum: 2 }, uniqueness: true 
+    validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*(\.[a-z]+)?\z/i, message: "use a valid email" }
+end

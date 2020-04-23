e38e4f7813ebc13fd57456132433b72b161ec800
enelesmai <xochitl.selene.fp@gmail.com>
2020-04-23 16:27:39 -0500

    added rubocop and stickler configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-    has_many :posts
-    has_many :comments
-    validates :name, presence: true, length: { minimum: 2 }, uniqueness: true 
-    validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*(\.[a-z]+)?\z/i, message: "use a valid email" }
+  has_many :posts
+  has_many :comments
+  validates :name, presence: true, length: { minimum: 2 }, uniqueness: true
+  validates :email, presence: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*(\.[a-z]+)?\z/i, message: 'use a valid email' }
 end
